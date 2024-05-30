dd52126d1ed4b0c013bc00bc1fd279cd74d6ef54
Kaleto13 <kaledconk@gmail.com>
2024-05-30 19:16:50 -0400

    arreglo de bug

--- Proyecto/base/urls.py
+++ Proyecto/base/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns=[
     path('', views.home, name="home"),
-    path('createacount', views.createacount, name="createacount")
+    path('createacount', views.createacount, name="createacount"),
     path('cambiarcontraseña', views.cambiarcontraseña, name="cambiarcontraseña")
 
 ]
